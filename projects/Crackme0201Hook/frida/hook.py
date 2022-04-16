63b7ab3089eef985ccae5d25be7714845aa8f248
赵坤 <zk9474@hotmail.com>
2022-04-16 18:36:03 +0800

    frida hook

--- projects/Crackme0201Hook/frida/hook.py
+++ projects/Crackme0201Hook/frida/hook.py
@@ -5,8 +5,11 @@ Java.perform(function () {
     //这里是要Hook的软件具体位置的   包名 + 类名
     var mainActivity = Java.use('com.neo.crackme0201.MainActivity');
     //这里写要Hook的方法名  以及参数
-    mainActivity.checkSn.implementation=function(str){
+    mainActivity.checkSn.implementation=function(username, sn){
         //直接返回为true
+        console.log(username, 'username')
+        console.log(sn, 'sn')
+        send(sn)
         return true;
     };
 });
@@ -14,11 +17,14 @@ Java.perform(function () {
 
 
 def on_message(message, data):
-    print(message)
+    if message['type'] == 'send':
+        print("[*] {0}".format(message['payload']))
+    else:
+        print(message)
 
-process = frida.get_usb_device().attach('com.neo.crackme0201')  # 要Hook的软件包名
+process = frida.get_usb_device().attach('Crackme0201')  # 要Hook的软件包名
 script = process.create_script(jscode)
 script.on('message', on_message)
-print('运行完毕！！！请随便输入密码进入软件。')
+print('hook 成功。')
 script.load()
 sys.stdin.read()
